509a5d129b8d1784183890a68446c9f2dc8ddd6b
Ritika <ritikakc223@gimal.com>
2026-01-04 08:53:36 +0545

    change in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const app = require("./app");  // get app
+import app from "./app.js";
 
-const PORT = 5000;  // port is like a door
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
